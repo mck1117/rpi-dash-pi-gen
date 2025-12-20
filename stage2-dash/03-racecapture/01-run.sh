751591d0c5b65d29b95bff2f18cab467ca618426
Matthew Kennedy <matthewkennedy@outlook.com>
2025-12-20 04:46:09 -0800

    fetch racecapture deb

--- stage2-dash/03-racecapture/01-run.sh
+++ stage2-dash/03-racecapture/01-run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
-dpkg-deb -x files/racecapture_linux_raspberrypi_2.17.1.deb ${ROOTFS_DIR}
+# RC_TEMP="$(mktemp)"
+RC_TEMP=files/racecapture.deb
+wget -O "$RC_TEMP" https://autosportlabs-software.s3.us-west-2.amazonaws.com/racecapture_linux_raspberrypi_2.17.1.deb
+dpkg-deb -x $RC_TEMP ${ROOTFS_DIR}
+# rm -rf $RC_TEMP
 
 install -m 644 files/racecapture.service "${ROOTFS_DIR}/etc/systemd/system/"
 
