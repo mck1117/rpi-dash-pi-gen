6acf55820f051f5f14ffd5e2d04f446000711405
Matthew Kennedy <matthewkennedy@outlook.com>
2025-12-29 22:34:08 -0800

    bypass the racecapture rpi script

--- stage2-racecapture/00-racecapture/files/run_racecapture_wrapper.sh
+++ stage2-racecapture/00-racecapture/files/run_racecapture_wrapper.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libmount.so.1 /opt/racecapture/run_racecapture_rpi.sh -- -c graphics:show_cursor:0 -a -m cursor -c kivy:keyboard_mode:systemandmulti -c graphics:rotation:90
+cd /opt/racecapture
+LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libmount.so.1 ./race_capture -c graphics:show_cursor:0 -a -m cursor -c kivy:keyboard_mode:systemandmulti -c graphics:rotation:90
