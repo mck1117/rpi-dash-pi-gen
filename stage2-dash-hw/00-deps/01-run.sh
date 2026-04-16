d83ebfc624fc3617a003788145828e35cab1ab76
Matthew Kennedy <matthewkennedy@outlook.com>
2026-04-16 01:23:40 -0700

    remove cloud-init

--- stage2-dash-hw/00-deps/01-run.sh
+++ stage2-dash-hw/00-deps/01-run.sh
@@ -1,5 +1,5 @@
 # remove stuff we don't need
 
 on_chroot << EOF
-    apt remove modemmanager avahi* bluez* wpasupplicant -y
+    apt remove modemmanager avahi* bluez* wpasupplicant cloud-init -y
 EOF
