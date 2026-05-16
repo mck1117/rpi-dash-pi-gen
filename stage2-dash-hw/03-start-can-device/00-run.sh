137dc5ee6ed1a26368416e337dec5a0995cb0648
Matthew Kennedy <matthewkennedy@outlook.com>
2026-05-15 23:53:15 -0700

    enable systemd-networkd

--- stage2-dash-hw/03-start-can-device/00-run.sh
+++ stage2-dash-hw/03-start-can-device/00-run.sh
@@ -2,3 +2,7 @@
 
 install -m 644 files/80-can.network "${ROOTFS_DIR}/etc/systemd/network/"
 install -m 644 files/80-can-hotplug.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
+
+on_chroot << EOF
+    systemctl enable systemd-networkd
+EOF
