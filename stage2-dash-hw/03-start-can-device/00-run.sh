51dd15e64e8367d5160639d9ab818c4aab9a36cd
Matthew Kennedy <matthewkennedy@outlook.com>
2026-07-28 00:10:05 -0700

    don't wait to be online

--- stage2-dash-hw/03-start-can-device/00-run.sh
+++ stage2-dash-hw/03-start-can-device/00-run.sh
@@ -5,4 +5,6 @@ install -m 644 files/80-can-hotplug.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
 
 on_chroot << EOF
     systemctl enable systemd-networkd
+
+    systemctl disable NetworkManager-wait-online.service
 EOF
